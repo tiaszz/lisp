20afb2712aa7894c52297b3d2745d6428d0e00e6
tiasz <iskiizeh3@gmail.com>
2024-11-27 15:56:15 -0300

    now its working

--- prompt.c
+++ prompt.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/realine.h>
 #include <editline/history.h>
+#include <editline/readline.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   puts("Lispy version 0.0.0.0.1");
   puts("Press Ctrl+c to exit\n");
 
   while (1) {
     /* Output our prompt and get input */
-    char* input = realine("lispy> ");
+    char *input = readline("lispy> ");
 
     add_history(input);
 
